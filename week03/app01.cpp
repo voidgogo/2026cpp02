383c35fb0c3ab7fecd67b5c3e0c328b548e0b50a
voidgogo <2021038@daelim.ac.kr>
2026-09-17 16:13:16 +0900

    week03: Polymorphism v0.3

--- week03/app01.cpp
+++ week03/app01.cpp
@@ -1,28 +1,28 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-class Pokemon
-{
-public:
-	virtual void attack() const { cout << "포켓몬 몸통박치기" << endl; }
-};
-class Pikachu : public Pokemon
-{
-public:
-	void attack() const { cout << "피카츄 10만 볼트" << endl; }
-};
-int main()
-{
-	Pokemon* ptr;
-	ptr = new Pokemon();
-	ptr->attack();
-	delete ptr;
-	ptr = nullptr;
-
-	ptr = new Pikachu();
-	ptr->attack();
-	delete ptr;
-	ptr = nullptr;
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//class Pokemon
+//{
+//public:
+//	virtual void attack() const { cout << "포켓몬 몸통박치기" << endl; }
+//};
+//class Pikachu : public Pokemon
+//{
+//public:
+//	void attack() const { cout << "피카츄 10만 볼트" << endl; }
+//};
+//int main()
+//{
+//	Pokemon* ptr;
+//	ptr = new Pokemon();
+//	ptr->attack();
+//	delete ptr;
+//	ptr = nullptr;
+//
+//	ptr = new Pikachu();
+//	ptr->attack();
+//	delete ptr;
+//	ptr = nullptr;
+//	return 0;
+//}
---
+++ week03/app02.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Pokemon
+{
+public:
+	//Pokemon() { cout << "포켓몬 기본 생성자\n"; }
+	~Pokemon(){ cout << "포켓몬 객체 소멸\n"; }
+	virtual void attack() const { cout << "포켓몬 몸통박치기" << endl; }
+};
+class Pikachu : public Pokemon
+{
+public:
+	//Pikachu() { cout << "피카츄 기본 생성자\n"; }
+	~Pikachu() { cout << "피카츄 객체 소멸\n"; }
+	void attack() const { cout << "피카츄 10만 볼트" << endl; }
+};
+int main()
+{
+	Pokemon* ptr;
+	ptr = new Pokemon();
+	ptr->attack();
+	delete ptr;
+	ptr = nullptr;
+
+	ptr = new Pikachu();
+	ptr->attack();
+	delete ptr;
+	ptr = nullptr;
+	return 0;
+}
